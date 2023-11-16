41a7808bc90d048d87de579efee3a11c020e9fc1
Elcrackeadordecracks <haz978@hotmail.com>
2023-11-16 08:31:41 -0600

    Agregado método PATCH  en index.php

--- procesar_compra.php
+++ procesar_compra.php
@@ -1,29 +1,28 @@
 <?php
 include("config.php");
 
-// Verificar si la solicitud es PUT
-if ($_SERVER["REQUEST_METHOD"] == "PUT") {
+// Verificar si la solicitud es PATCH
+if ($_SERVER["REQUEST_METHOD"] == "PATCH") {
     // Leer los datos del cuerpo de la solicitud
-    $datos_put = json_decode(file_get_contents("php://input"), true);
+    $datos_patch = json_decode(file_get_contents("php://input"), true);
 
-    // Obtener los datos del formulario PUT
-    $producto_id = $datos_put['producto_id'];
-    $nombre = $datos_put['nombre'];
-    $email = $datos_put['email'];
-    $cantidad = $datos_put['cantidad'];
+    // Obtener los datos del formulario PATCH
+    $producto_id = $datos_patch['producto_id'];
+    $nombre = $datos_patch['nombre'];
+    // Otros campos que puedas necesitar actualizar
 
-    // Realizar operaciones con los datos obtenidos del formulario PUT
-    // Por ejemplo, puedes guardarlos en la base de datos o realizar otras acciones necesarias
+    // Realizar operaciones con los datos obtenidos del formulario PATCH
+    // Por ejemplo, puedes actualizar la información en la base de datos
 
-    // Ejemplo de inserción en la base de datos (esto debe adaptarse según tu estructura de base de datos)
-    $sql = "INSERT INTO compras (producto_id, nombre, email, cantidad) VALUES ('$producto_id', '$nombre', '$email', '$cantidad')";
+    // Ejemplo de actualización en la base de datos (esto debe adaptarse según tu estructura de base de datos)
+    $sql = "UPDATE compras SET nombre = '$nombre' WHERE producto_id = $producto_id";
 
     if ($conn->query($sql) === TRUE) {
-        echo "Compra realizada con éxito. Gracias por su compra.";
+        echo "Compra actualizada con éxito.";
     } else {
-        echo "Error al procesar la compra: " . $conn->error;
+        echo "Error al actualizar la compra: " . $conn->error;
     }
 } else {
-    echo "Acceso no autorizado.";
+    // ... (resto del código para manejar otras solicitudes, si es necesario)
 }
 ?>
